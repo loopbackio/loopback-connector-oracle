e9d2371d6e29dbbeedea84b57beffe14a831e53e
Raymond Feng <raymond@strongloop.com>
2013-04-10 08:11:59 -0700

    Initial commit

---
+++ test/init.js
@@ -0,0 +1,12 @@
+var Schema = require('jugglingdb').Schema;
+
+global.getSchema = function() {
+    var db = new Schema(require('../'), {
+        host:'166.78.158.45',
+        database:'XE',
+        username:'strongloop',
+        password:'str0ng100pjs'
+    });
+    db.log = function (a) { console.log(a); };
+    return db;
+};
