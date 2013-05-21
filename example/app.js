d362772ee818450e3e49dd02c985c010538064d9
Raymond Feng <raymond@strongloop.com>
2013-05-21 11:30:36 -0700

    Update discovery json format

--- example/app.js
+++ example/app.js
@@ -8,24 +8,21 @@ var db = new Schema(require('../'), {
   debug : true
 });
 
+function show(err, models) {
+    if (err) {
+        console.error(err);
+    } else {
+        models.forEach(function(m) {
+            console.dir(m);
+        });
+    }
+}
 
-db.discoverModels({views: true, limit: 3}, function(err, models) {
-  if (err) {
-    console.error(err);
-  } else {
-    models.forEach(function(m) {
-      console.dir(m);
-    });
-  }
-});
+db.discoverModels({views: true, limit: 3}, show);
 
+db.discoverModelProperties({model: 'PRODUCT'}, show);
 
-db.discoverModelProperties({model: 'PRODUCT'}, function(err, models) {
-  if (err) {
-    console.error(err);
-  } else {
-    models.forEach(function(m) {
-      console.dir(m);
-    });
-  }
-});
+db.discoverModelProperties({model: 'INVENTORY_VIEW'}, show);
+
+db.discoverPrimaryKeys(null, 'INVENTORY',  show);
+db.discoverForeignKeys(null, 'INVENTORY',  show);
