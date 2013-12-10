8988fdf90c64b4353001ad0a5f7f64162a922e5f
Ryan Graham <ryan@strongloop.com>
2013-12-10 13:06:05 -0800

    Run tests more similar to how CI runs them

--- test/init/init.js
+++ test/init/init.js
@@ -3,9 +3,9 @@ var DataSource = require('loopback-datasource-juggler').DataSource;
 var config = require('rc')('loopback', {test: {oracle: {}}}).test.oracle;
 
 global.getDataSource = global.getSchema = function() {
-    var db = new DataSource(require('../'), config);
-    db.log = function (a) { 
-	    // console.log(a); 
+    var db = new DataSource(require('../../'), config);
+    db.log = function (a) {
+	    // console.log(a);
     };
     return db;
 };
