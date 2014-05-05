7c55483e168cfb475ad766110b49c6bf57dc5ed7
Raymond Feng <raymond@strongloop.com>
2014-05-05 13:51:38 -0700

    Reformat code

--- test/init/init.js
+++ test/init/init.js
@@ -2,10 +2,10 @@ var DataSource = require('loopback-datasource-juggler').DataSource;
 
 var config = require('rc')('loopback', {test: {oracle: {}}}).test.oracle;
 
-global.getDataSource = global.getSchema = function() {
-    var db = new DataSource(require('../../'), config);
-    db.log = function (a) {
-	    // console.log(a);
-    };
-    return db;
+global.getDataSource = global.getSchema = function () {
+  var db = new DataSource(require('../../'), config);
+  db.log = function (a) {
+    // console.log(a);
+  };
+  return db;
 };
